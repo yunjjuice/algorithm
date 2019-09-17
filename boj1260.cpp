464fe4e11fc6f4920a3867c6180d1e3751a6a0a7
최윤주 <yeomju311@gmail.com>
2019-09-18 01:31:24 +0900

    initial commit

---
+++ boj1260.cpp
@@ -0,0 +1,41 @@
+//
+//  boj1260.cpp
+//  Algorithm
+//
+//  Created by 최윤주 on 2019. 9. 9..
+//  Copyright © 2019년 YUNJU. All rights reserved.
+//
+
+/*
+ #include <iostream>
+ #include <queue>
+ #include <vector>
+ 
+ using namespace std;
+ 
+ vector<int> vec[1001];
+ 
+ int  dfs() { // 깊이우선탐색, 스택, 재귀
+ 
+ return 0;
+ }
+ 
+ int bfs() { // 너비우선탐색, 큐
+ 
+ return 0;
+ }
+ 
+ int main() {
+ int n, m, v; // 정점개수, 간선개수, 시작정점
+ cin >> n >> m >> v;
+ 
+ for (int i = 0 ; i < m ; i++){
+ int a, b;
+ cin >> a >> b;
+ vec[a].push_back(b);
+ vec[b].push_back(a);
+ }
+ 
+ return 0;
+ }
+*/
---
+++ main.cpp
@@ -0,0 +1,8 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 최윤주 on 2019. 9. 5..
+//  Copyright © 2019년 YUNJU. All rights reserved.
+//
+
