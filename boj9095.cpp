ada174fb06effe23e0cb5a8687ba348583eba128
최윤주 <yeomju311@gmail.com>
2019-09-29 19:47:17 +0900

    9095 1,2,3 더하기

--- boj9095.cpp
+++ boj9095.cpp
@@ -6,4 +6,40 @@
 //  Copyright © 2019년 YUNJU. All rights reserved.
 //
 
-#include <stdio.h>
+
+#include <iostream>
+
+using namespace std;
+
+int solution(int sum, int goal){
+	
+	if(sum > goal)
+		return 0;
+	if(sum == goal)
+		return 1;
+	
+	int tmp = 0;
+	for(int i = 1 ; i <= 3 ; i++){
+		tmp += solution(sum+i, goal);
+	}
+	
+	return tmp;
+}
+
+int main() {
+    
+    int t;
+	cin >> t;
+	
+	int a[t];
+	
+	int sum = 0;
+	int count;
+	
+	for(int i = 0 ; i < t ; i++){
+		count = 0;
+		cin >> a[i];
+		count = solution(sum, a[i]);
+		cout << count << "\n";
+	}
+}
