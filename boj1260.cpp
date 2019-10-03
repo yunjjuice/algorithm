6df942ab5640933bfb5fc8366b977590707ace68
최윤주 <yeomju311@gmail.com>
2019-10-04 01:21:43 +0900

    1260 DFS와 BFS

--- boj1260.cpp
+++ boj1260.cpp
@@ -6,36 +6,67 @@
 //  Copyright © 2019년 YUNJU. All rights reserved.
 //
 
-/*
- #include <iostream>
- #include <queue>
- #include <vector>
+
+
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int n, m, v; // n : 정점의 개수, m : 간선 개수, v : 시작할 정점의 번호
+bool check1[1001]; // 방문확인
+bool check2[1001];
+vector<int> a[1001];
+
+void dfs(int x) {
+    check1[x] = true;
+    printf("%d ", x);
+    for(int i = 0 ; i < a[x].size() ; i++){
+        int y = a[x][i];
+        if(check1[y] != true)
+            dfs(y);
+    }
+}
+
+void bfs(int start) {
+    queue<int> q;
+    q.push(start);
+    check2[start] = true;
+    
+    while(!q.empty()){
+        int x = q.front();
+        q.pop();
+        printf("%d ", x);
+        for(int i = 0 ; i < a[x].size() ; i++){
+            int y = a[x][i];
+            if(!check2[y]){
+                q.push(y);
+                check2[y] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d %d", &n, &m, &v);
+    
+    for(int i = 0 ; i < m ; i++){
+        int x, y;
+        scanf("%d %d", &x, &y);
+        a[x].push_back(y);
+        a[y].push_back(x);
+    }
+    
+    for(int i = 1 ; i <= n ; i++){
+        sort(a[i].begin(), a[i].end());
+    }
+    
+    dfs(v);
+    printf("\n");
+    bfs(v);
+    printf("\n");
+    
+    return 0;
+}
  
- using namespace std;
- 
- vector<int> vec[1001];
- 
- int  dfs() { // 깊이우선탐색, 스택, 재귀
- 
- return 0;
- }
- 
- int bfs() { // 너비우선탐색, 큐
- 
- return 0;
- }
- 
- int main() {
- int n, m, v; // 정점개수, 간선개수, 시작정점
- cin >> n >> m >> v;
- 
- for (int i = 0 ; i < m ; i++){
- int a, b;
- cin >> a >> b;
- vec[a].push_back(b);
- vec[b].push_back(a);
- }
- 
- return 0;
- }
-*/
