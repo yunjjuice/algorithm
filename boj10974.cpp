3a132a9fc88177a191b0d6bc8d50771396dab94b
최윤주 <yeomju311@gmail.com>
2019-10-09 15:05:46 +0900

    10974 모든 순열

---
+++ boj10974.cpp
@@ -0,0 +1,33 @@
+//
+//  boj10974.cpp
+//  Algorithm
+//
+//  Created by 최윤주 on 2019. 10. 9..
+//  Copyright © 2019년 YUNJU. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> v(n);
+    
+    for(int i = 1 ; i <= n ; i++){
+        v[i-1] = i;
+    }
+    
+    do {
+        for(int i = 0 ; i < n ; i++){
+            cout << v[i] << ' ';
+        }
+        cout << '\n';
+    } while(next_permutation(v.begin(), v.end()));
+    
+    return 0;
+}
