400d0a0908b9e09194ef3d80f425c05d4941a917
최윤주 <yeomju311@gmail.com>
2019-10-11 17:34:40 +0900

    2588 곱셈

---
+++ boj2588.cpp
@@ -0,0 +1,25 @@
+//
+//  boj2588.cpp
+//  Algorithm
+//
+//  Created by 최윤주 on 2019. 10. 11..
+//  Copyright © 2019년 YUNJU. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+	int a, b3, b2, b1;
+	scanf("%d %1d%1d%1d", &a, &b3, &b2, &b1);
+	
+	int tmp1 = a*b1;
+	int tmp2 = a*b2;
+	int tmp3 = a*b3;
+	
+	printf("%d\n", tmp1);
+	printf("%d\n", tmp2);
+	printf("%d\n", tmp3);
+	printf("%d\n", tmp1 + tmp2*10 + tmp3*100);
+	
+	return 0;
+}
