bfb6e887fdcaeff67eb6e2d9f371edbbaf286a0d
최윤주 <yeomju311@gmail.com>
2019-10-18 23:19:00 +0900

    15650 N과 M (2)

---
+++ boj15650.cpp
@@ -0,0 +1,37 @@
+//
+//  boj15650.cpp
+//  Algorithm
+//
+//  Created by 최윤주 on 2019. 10. 18..
+//  Copyright © 2019년 YUNJU. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int a[10];
+int n, m;
+
+void solution(int index, int start){
+	if(index == m){
+		for(int i=0 ; i<m ; i++){
+			cout << a[i];
+			if(i != m-1)
+				cout << ' ';
+		}
+		cout << '\n';
+	}
+	
+	for(int i=start ; i<=n ; i++){
+		a[index] = i;
+		solution(index+1, i+1);
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	solution(0, 1);
+	
+	return 0;
+}
