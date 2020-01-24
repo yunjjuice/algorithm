a53e4a2b190d5a19f297b978454bb0a7adbaab9a
최윤주 <yeomju311@gmail.com>
2020-01-24 17:09:48 +0900

    2231 분해합

--- BAEKJOON/BOJ2231.java
+++ BAEKJOON/BOJ2231.java
@@ -1,11 +1,8 @@
-package algorithmStudy;
-
 import java.util.Scanner;
 
 public class BOJ2231 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 
 		int N = sc.nextInt();
