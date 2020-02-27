01703ada8b2ed3516fb53fad014209794af9f7d3
최윤주 <yeomju311@gmail.com>
2020-02-28 00:40:52 +0900

    BOJ 15649 N과 M (1)

---
+++ BAEKJOON/BOJ15649.java
@@ -0,0 +1,47 @@
+/*
+ * BOJ 15649 N과 M (1)
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15649 {
+	static int n, m;
+	static int[] arr;
+	static boolean[] used;
+	static int[] pick;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n+1];
+		used = new boolean[n+1];
+		pick = new int[m+1];
+		for (int i = 1; i <= n; i++) {
+			arr[i] = i;
+		}
+		// 1~N까지 자연수 중에 M개 선택
+		solve(0);
+	}
+		
+	static void solve(int index) {
+		if(index == m) {
+			for (int i = 0; i < m; i++) {
+				System.out.print(pick[i] + " ");
+			}
+			System.out.println();
+			return;
+		} 
+		for (int i = 1; i < arr.length; i++) {
+			if(!used[i]){
+				pick[index] = i;
+				used[i] = true;
+				solve(index+1);
+				used[i] = false;
+			}
+		}
+	}
+}
