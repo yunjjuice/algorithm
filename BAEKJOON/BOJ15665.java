36fe2685ef966d4f3ae4d9c30817a4c19383e895
최윤주 <yeomju311@gmail.com>
2020-03-01 02:09:23 +0900

    BOJ 15665 N과 M (11)

---
+++ BAEKJOON/BOJ15665.java
@@ -0,0 +1,45 @@
+/*
+ * BOJ 15665 N과 M (11)
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15665 {
+	static int n, m;
+	static int[] a, pick;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		a = new int[10001];
+		pick = new int[m];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < n; i++) {
+			a[Integer.parseInt(st.nextToken())]++;
+		}
+		solve(0);
+		System.out.println(sb);
+	}
+	
+	static void solve(int index) {
+		if(index == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(pick[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 1; i < 10001; i++) {
+			if(a[i] == 0)
+				continue;
+			pick[index] = i;
+			solve(index+1);
+		}
+	}
+}
