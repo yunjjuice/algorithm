7a9420ca09e16e8dc9cdd508a530d97acf73dfa1
최윤주 <yeomju311@gmail.com>
2021-04-10 21:48:36 +0900

    BOJ 4179 불! Gold 4

---
+++ BAEKJOON/BOJ4179.java
@@ -0,0 +1,95 @@
+/*
+ * BOJ 4179 불!
+ * Gold 4
+ * https://www.acmicpc.net/problem/4179
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ4179 {
+	static int R, C, ans;
+	static char[][] map;
+	static boolean[][] visited;
+	static Queue<Pos> person, fire;
+	static int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new char[R+2][C+2]; // 탈출 확인을 쉽게 하기 위해 +2씩 크게 만듦 
+		visited = new boolean[R+2][C+2];
+		person = new LinkedList<>();
+		fire = new LinkedList<>();
+		for (int i = 1; i <= R; i++) {
+			String str = br.readLine();
+			for (int j = 1; j <= C; j++) {
+				map[i][j] = str.charAt(j-1);
+				if (map[i][j] == 'J') {
+					person.offer(new Pos(i, j));
+					visited[i][j] = true;
+					map[i][j] = '.';
+				} else if (map[i][j] == 'F') {
+					fire.offer(new Pos(i, j));
+				}
+			}
+		}
+		
+		ans = Integer.MAX_VALUE;
+		bfs();
+		if (ans == Integer.MAX_VALUE) {
+			System.out.println("IMPOSSIBLE");
+		} else {
+			System.out.println(ans);
+		}
+	}
+	
+	public static void bfs() {
+		int time = 0;
+		while (!person.isEmpty()) {
+			// 불이 먼저 퍼진다
+			int qs = fire.size();
+			for (int s = 0; s < qs; s++) {
+				Pos cur = fire.poll();
+				for (int i = 0; i < 4; i++) {
+					int nx = cur.x + dir[i][0];
+					int ny = cur.y + dir[i][1];
+					if (nx <= 0 || R+1 <= nx || ny <= 0 || C+1 <= ny) continue;
+					if (map[nx][ny] == 'F' || map[nx][ny] == '#') continue;
+					fire.offer(new Pos(nx, ny));
+					map[nx][ny] = 'F';
+				}
+			}
+			
+			// 사람이 이동한다
+			qs = person.size();
+			for (int s = 0; s < qs; s++) {
+				Pos cur = person.poll();
+				if (cur.x == 0 || cur.x == R+1 || cur.y == 0 || cur.y == C+1) {
+					ans = Math.min(ans, time);
+					return;
+				}
+				for (int i = 0; i < 4; i++) {
+					int nx = cur.x + dir[i][0];
+					int ny = cur.y + dir[i][1];
+					if (map[nx][ny] == 'F' || map[nx][ny] == '#') continue;
+					if (visited[nx][ny]) continue;
+					person.offer(new Pos(nx, ny));
+					visited[nx][ny] = true;
+				}
+			}
+			time++;
+		}
+	}
+	
+	static class Pos {
+		int x, y;
+
+		public Pos(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
